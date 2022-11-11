53b0b248d7671771816ad2a28e0245701e378b71
Osehi <osehiaseehilen@gmail.com>
2022-11-11 11:37:33 +0100

    Added background

--- app/src/main/java/com/polish/resource/features/search/SearchFragment.kt
+++ app/src/main/java/com/polish/resource/features/search/SearchFragment.kt
@@ -1,32 +1,24 @@
 package com.polish.resource.features.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.polish.resource.R
 
-
-
-
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
-
-
 }
